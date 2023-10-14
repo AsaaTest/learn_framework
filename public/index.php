3dba06a97b75daac225e480df0a6574027a2bee1
AsaaTest <abelcoc06@gmail.com>
2023-10-14 14:03:02 -0500

    update(index):
    use app, bootstrap, and comment code

--- public/index.php
+++ public/index.php
@@ -1,30 +1,33 @@
 <?php
-// import required class
-
+// Import the required classes and namespaces.
 use Learn\App;
-use Learn\Http\HttpNotFoundException;
 use Learn\Http\Request;
 use Learn\Http\Response;
-use Learn\Routing\Router;
-use Learn\Server\PhpNativeServer;
 
-// require autoload of composer
+// Require the Composer autoload file.
 require_once '../vendor/autoload.php';
-// require helpers
+
+// Require custom helper functions.
 require_once '../src/Helpers/helpers.php';
 
-//define class Router
-$app = new App();
-// define routes
+// Bootstrap the application, initializing it.
+$app = App::bootstrap();
+
+// Define routes using the application's router.
 $app->router->get('/test/{test}', function (Request $request) {
+    // Define a route that responds to GET requests with JSON data containing route parameters.
     return Response::json($request->routeParameters());
 });
 
 $app->router->get('/redirect', function (Request $request) {
+    // Define a route that responds to GET requests by redirecting to another route.
     return Response::redirect('/test/4');
 });
+
 $app->router->post('/test', function (Request $request) {
+    // Define a route that responds to POST requests with JSON data containing query parameters.
     return Response::json($request->query());
 });
 
+// Run the application, which will handle incoming HTTP requests based on the defined routes.
 $app->run();
