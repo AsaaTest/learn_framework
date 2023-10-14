d5eab903e03a43c02f46a5f5c58dac9c887e07c2
AsaaTest <abelcoc06@gmail.com>
2023-10-13 23:52:46 -0500

    feat(PhpNativeServer):
    create a Server native of php, obtain parameters of $_SERVER, _POST, _GET

---
+++ src/Learn/PhpNativeServer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Learn;
+
+class PhpNativeServer implements Server
+{
+    /**
+     * Get Uri of Server
+     *
+     * @return string
+     */
+    public function requestUri(): string
+    {
+        return parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    }
+
+    /**
+     * Get method of Server
+     *
+     * @return string
+     */
+    public function requestMethod(): string
+    {
+        return $_SERVER["REQUEST_METHOD"];
+    }
+
+    /**
+     * Ger Post data send by route
+     *
+     * @return array
+     */
+    public function postData(): array
+    {
+        return $_POST;
+    }
+
+    /**
+     * Get data of _GET send in route
+     *
+     * @return array
+     */
+    public function queryParams(): array
+    {
+        return $_GET;
+    }
+}
