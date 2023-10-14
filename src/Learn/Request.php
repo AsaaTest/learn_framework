e46e7e1f8ba1e38c8ac0eeaa46c422b4230af656
AsaaTest <abelcoc06@gmail.com>
2023-10-13 23:27:32 -0500

    feat(Request):
    create class Request, get uri, method, data (post and get) of server

---
+++ src/Learn/Request.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Learn;
+
+class Request
+{
+    protected string $uri;
+
+    protected string $method;
+
+    protected array $data;
+
+    protected array $query;
+
+    public function __construct(Server $server)
+    {
+        $this->uri = $server->requestUri();
+        $this->method = $server->requestMethod();
+        $this->data = $server->postData();
+        $this->query = $server->queryParams();
+    }
+
+    public function uri(): string
+    {
+        return $this->uri;
+    }
+
+    public function method(): string
+    {
+        return $this->method;
+    }
+}
