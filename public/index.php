f34f67b2e82d22a734c07d63f978e3828b375536
AsaaTest <abelcoc06@gmail.com>
2023-10-15 23:28:19 -0500

    update(index):
    create a route for validate

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use Learn\Http\Middleware;
 use Learn\Http\Request;
 use Learn\Http\Response;
 use Learn\Routing\Route;
+use Learn\Validation\Rule;
+use Learn\Validation\Rules\Required;
 
 // Require the Composer autoload file.
 require_once '../vendor/autoload.php';
@@ -33,7 +35,7 @@ class AuthMiddleware implements Middleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if($request->headers('Authorization') != 'test') {
+        if ($request->headers('Authorization') != 'test') {
             return json(['msg' => 'not autorizated'])->setStatus(401);
         }
         $response = $next($request);
@@ -47,6 +49,19 @@ Route::get('/middlewares', fn (Request $request) => json(['msg' => 'ok middlewar
     ->setMiddlewares([AuthMiddleware::class]);
 
 Route::get('/html', fn (Request $request) => view('home', ['user' => 'Pedro']));
+
+Route::post('/validate', fn (Request $request) => json($request->validate(
+    [
+        'test' => Rule::required(),
+        'num' => Rule::number(),
+        'email' => [Rule::required(), Rule::email()]
+    ],
+    [
+        'email' => [
+            Required::class => 'DAME EL CAMPO'
+        ]
+    ]
+)));
 
 // Run the application, which will handle incoming HTTP requests based on the defined routes.
 $app->run();
