95cb1f05e1b6e5845c391f9bf210a3760723dcab
AsaaTest <abelcoc06@gmail.com>
2023-10-16 00:34:43 -0500

    feat(ValidatorTest):
    create test for validator

---
+++ tests/Validation/ValidatorTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Learn\Tests\Validation;
+
+use Learn\Validation\Exceptions\ValidationException;
+use Learn\Validation\Rule;
+use Learn\Validation\Rules\Email;
+use Learn\Validation\Rules\LessThan;
+use Learn\Validation\Rules\Number;
+use Learn\Validation\Rules\Required;
+use Learn\Validation\Rules\RequiredWith;
+use Learn\Validation\Validator;
+use PHPUnit\Framework\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        Rule::loadDefaultRules();
+    }
+
+    public function test_basic_validation_passes()
+    {
+        $data = [
+            "email" => "test@test.com",
+            "other" => 2,
+            "num" => 3,
+            "foo" => 5,
+            "bar" => 4
+        ];
+
+        $rules = [
+            "email" => new Email(),
+            "other" => new Required(),
+            "num" => new Number(),
+        ];
+
+        $expected = [
+            "email" => "test@test.com",
+            "other" => 2,
+            "num" => 3,
+        ];
+
+        $v = new Validator($data);
+
+        $this->assertEquals($expected, $v->validate($rules));
+    }
+
+    public function test_throws_validation_exception_on_invalid_data()
+    {
+        $this->expectException(ValidationException::class);
+        $v = new Validator(["test" => "test"]);
+        $v->validate(["test" => new Number()]);
+    }
+
+    /**
+     * @depends test_basic_validation_passes
+     */
+    public function test_multiple_rules_validation()
+    {
+        $data = ["age" => 20, "num" => 3, "foo" => 5];
+
+        $rules = [
+            "age" => new LessThan(100),
+            "num" => [new RequiredWith("age"), new Number()],
+        ];
+
+        $expected = ["age" => 20, "num" => 3];
+
+        $v = new Validator($data);
+
+        $this->assertEquals($expected, $v->validate($rules));
+    }
+
+    public function test_overrides_error_messages_correctly()
+    {
+        $data = ["email" => "test@", "num1" => "not a number"];
+
+        $rules = [
+            "email" => "email",
+            "num1" => "number",
+            "num2" =>  ["required", "number"],
+        ];
+
+        $messages = [
+            "email" => ["email" => "test email message"],
+            "num1" => ["number" => "test number message"],
+            "num2" =>  [
+                "required" => "test required message",
+                "number" => "test number message again"
+            ]
+        ];
+
+        $v = new Validator($data);
+
+        try {
+            $v->validate($rules, $messages);
+            $this->fail("Did not throw ValidationException");
+        } catch (ValidationException $e) {
+            $this->assertEquals($messages, $e->errors());
+        }
+    }
+}
