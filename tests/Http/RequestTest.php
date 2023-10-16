d85a7b2ca1732101677f73c36131447ec0db3844
AsaaTest <abelcoc06@gmail.com>
2023-10-15 21:09:12 -0500

    feat(RequestTest):
    add tests for return data with keys

--- tests/Http/RequestTest.php
+++ tests/Http/RequestTest.php
@@ -3,6 +3,7 @@
 namespace Learn\Tests\Http;
 
 use Learn\Http\Request;
+use Learn\Routing\Route;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
@@ -24,4 +25,56 @@ class RequestTest extends TestCase
         $this->assertEquals($postData, $request->data());
         $this->assertEquals('POST', $request->method());
     }
+
+    /**
+     * Test that the `data` method returns a value if a key is given.
+     */
+    public function test_data_returns_value_if_key_is_given()
+    {
+        // Define test data as an associative array.
+        $data = ['test' => 5, 'foo' => 1, 'bar' => 2];
+
+        // Create a request with the test data.
+        $request = (new Request())->setPostData($data);
+
+        // Perform assertions.
+        $this->assertEquals($request->data('test'), 5);   // Check if the 'test' key returns the expected value.
+        $this->assertEquals($request->data('foo'), 1);    // Check if the 'foo' key returns the expected value.
+        $this->assertNull($request->data("doesn't exist")); // Check if a non-existing key returns null.
+    }
+
+    /**
+     * Test that the `query` method returns a value if a key is given.
+     */
+    public function test_query_returns_value_if_key_is_given()
+    {
+        // Define test data as an associative array.
+        $data = ['test' => 5, 'foo' => 1, 'bar' => 2];
+
+        // Create a request with the test data as query parameters.
+        $request = (new Request())->setQueryParameters($data);
+
+        // Perform assertions.
+        $this->assertEquals($request->query('test'), 5);   // Check if the 'test' key returns the expected value.
+        $this->assertEquals($request->query('foo'), 1);    // Check if the 'foo' key returns the expected value.
+        $this->assertNull($request->query("doesn't exist")); // Check if a non-existing key returns null.
+    }
+
+    /**
+     * Test that the `routeParameters` method returns a value if a key is given.
+     */
+    public function test_route_parameters_returns_value_if_key_is_given()
+    {
+        // Define a route with parameters and a matching request.
+        $route = new Route('/test/{param}/foo/{bar}', fn () => "test");
+        $request = (new Request())
+            ->setRoute($route)
+            ->setUri('/test/1/foo/2');
+
+        // Perform assertions.
+        $this->assertEquals($request->routeParameters('param'), 1);   // Check if the 'param' key returns the expected value.
+        $this->assertEquals($request->routeParameters('bar'), 2);      // Check if the 'bar' key returns the expected value.
+        $this->assertNull($request->routeParameters("doesn't exist")); // Check if a non-existing key returns null.
+    }
+
 }
